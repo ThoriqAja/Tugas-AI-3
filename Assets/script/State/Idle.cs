163b555cac630590e2a9e045611d1312d36c37e8
ThoriqAja <athoriq1711@gmail.com>
2024-09-11 13:51:57 +0700

    Initial commit

---
+++ Assets/script/State/Idle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Idle : Grounded
+{
+    private float _horizontalInput;
+
+    public Idle(MovementSM stateMachine) : base("Idle", stateMachine) {}
+
+    public override void Enter()
+    {
+        base.Enter();
+        sm.spriteRenderer.color = Color.black; // Character turns black when idle
+        _horizontalInput = 0f;
+    }
+
+    public override void UpdateLogic()
+    {
+        base.UpdateLogic();
+        _horizontalInput = Input.GetAxis("Horizontal");
+
+        // Transition to Moving state if there is horizontal input
+        if (Mathf.Abs(_horizontalInput) > Mathf.Epsilon)
+        {
+            stateMachine.ChangeState(sm.movingState);
+        }
+    }
+}
